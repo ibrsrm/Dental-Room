2b4484035cd5368371fd9ba75ec56d1e308f2fc0
Ibrahim Serim <Ibrahim.Serim@airties.com>
2019-12-16 20:58:01 +0300

    Firebase Cloud Database Trigger Function

---
+++ CloudFunctions/index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const config = functions.config();
+admin.initializeApp(config.firebase);
+
+exports.newGroupCreated = functions.database.ref("/groups/{groupID}/members").onCreate((data, context) => {  
+      var groupID; 
+      var members = {};
+      groupID = context.params.groupID;
+      members = data.val();
+      for (var key in members) {
+	var group = {};
+	group[groupID] = 0;
+	admin.database().ref('/users/' + key).child('groups').update(group);
+      }
+      return 0; 
+});
+
